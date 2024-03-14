cfb3c55ab09860d4b22294e0d2bfedf276585df7
BenjaminWootten <bwootten@udel.edu>
2024-03-14 16:37:23 -0400

    destroy building feature started

--- UI/placement_preview.gd
+++ UI/placement_preview.gd
@@ -4,6 +4,8 @@ extends Area2D
 @onready var collisionShape = $CollisionShape2D
 
 var placing: bool
+var destroying: bool
+var last_collider: Node2D
 var building_to_place
 var colliding = false
 var offset = 0
@@ -13,6 +15,7 @@ const SNAP = 32
 @onready var conveyor_container = get_node("/root/World/conveyors")
 @onready var building_container = get_node("/root/World/machines")
 
+@export var destroy_texture: CompressedTexture2D
 @export var crusher_texture: CompressedTexture2D
 var crusher_path = preload("res://buildings/crafters/crusher.tscn")
 @export var conveyor_texture: CompressedTexture2D
@@ -22,18 +25,23 @@ var conveyor_path = preload("res://buildings/conveyors/conveyor.tscn")
 @export var hitbox_1x1: RectangleShape2D
 
 func _process(delta):
-	if placing:
+	if placing or destroying:
 		self.global_position = snapped(get_global_mouse_position(), Vector2(SNAP, SNAP))
 		position += Vector2(offset, offset)
 		
-		if Input.is_action_pressed("left_click") and not colliding:
-			var placement = building_to_place.instantiate()
-			if building_to_place == conveyor_path:
-				conveyor_container.add_child(placement)
-			else:
-				building_container.add_child(placement)
-			placement.position = global_position
-			placement.rotation = global_rotation
+		if placing:
+			if Input.is_action_pressed("left_click") and not colliding:
+				var placement = building_to_place.instantiate()
+				if building_to_place == conveyor_path:
+					conveyor_container.add_child(placement)
+				else:
+					building_container.add_child(placement)
+				placement.position = global_position
+				placement.rotation = global_rotation
+			
+		if destroying:
+			if Input.is_action_pressed("left_click") and colliding:
+				last_collider.queue_free()
 
 func placement_start(texture: CompressedTexture2D, hitbox: RectangleShape2D, newOffset: int, building: PackedScene):
 	placing = true
@@ -44,26 +52,35 @@ func placement_start(texture: CompressedTexture2D, hitbox: RectangleShape2D, new
 
 func placement_end():
 	placing = false
+	destroying = false
 	sprite.texture = null
 	collisionShape.shape = null
 
 func _input(event):
-	if placing:
+	if placing or destroying:
 		if event.is_action_pressed("quit_build"):
 			placement_end()
-		if event.is_action_pressed("rotate"):
+		if event.is_action_pressed("rotate") and placing:
 			rotation_degrees += 90
 
-func _on_body_entered(_body):
+func _on_body_entered(body):
 	entered_bodies += 1
 	colliding = true
 	sprite.modulate = Color(1,0,0)
+	last_collider = body
 
 func _on_body_exited(_body):
 	entered_bodies -= 1
 	if entered_bodies == 0:
 		colliding = false
 		sprite.modulate = Color(0,1,0)
+
+func _on_destroy_pressed():
+	destroying = true
+	sprite.texture = destroy_texture
+	collisionShape.shape = hitbox_1x1
+	offset = 0
+	sprite.scale = Vector2(0.25,0.25)
 
 func _on_conveyor_pressed():
 	placement_start(conveyor_texture, hitbox_1x1, 0, conveyor_path)
