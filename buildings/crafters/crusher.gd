c6a54658aed7a3541584743c9cbfad3f4543cb59
BenjaminWootten <bwootten@udel.edu>
2024-03-15 14:46:48 -0400

    updated recipes

--- buildings/crafters/crusher.gd
+++ buildings/crafters/crusher.gd
@@ -9,7 +9,9 @@ var output_inventory = 0
 
 var recipes = {
 	none = {input_type = "none", input_num = 0, output_type = "none", output_num = 0, time = 0},
-	iron_plate = {input_type = "iron", input_num = 1, output_type = "iron_plate", output_num = 1, time = 1},
+	lead_plate = {input_type = "lead_bar", input_num = 2, output_type = "lead_plate", output_num = 3, time = 3},
+	coal_powder = {input_type = "coal", input_num = 1, output_type = "coal_powder", output_num = 2, time = 2},
+	sulfur_powder = {input_type = "sulfur", input_num = 1, output_type = "sulfur_powder", output_num = 1, time = 2},
 }
 
 var recipe = recipes["none"]
